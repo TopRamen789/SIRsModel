fdcd1a45f02d85626dee62146a306f7eac599dea
FishFuzz99 <fishfuzz99@gmail.com>
2015-12-06 16:50:06 -0700

    updated methods to work with active Roster and non reference calls

--- SirsModel/GridUnit.cs
+++ SirsModel/GridUnit.cs
@@ -8,13 +8,57 @@ namespace SirsModel
 {
     class GridUnit
     {
+
+        private double _s;
+        private double _i;
+        private double _r;
+
         public long S { get; set; } // number of susceptible individuals
         public long I { get; set; } // number of infected individuals
         public long R { get; set; } // number of recovered individuals
+        public long D { get; set; } // number of deaths
         public long N { get; set; } // total Population, initially set by population density info
+        public int xCoord;
+        public int yCoord;
 
+        public double s
+        {
+            get { return _s; }
+            set
+            {
+                _s = value;
+                S = Convert.ToInt64(s * N);
+            } 
+        }
 
+        public double i {
+            get { return _i; }
+            set
+            {
+                _i = value;
+                I = Convert.ToInt64(i * N);
+            }
+        }
+        public double r {
+            get { return _r; }
+            set
+            {
+                _r = value;
+                R = Convert.ToInt64(r * N);
+            }
+        }
 
+        public GridUnit(long S, long I, long R, long N, int xCoord, int yCoord)
+        {
+            this.s = S / N;
+            this.i = S / N;
+            this.r = R / N;
+            this.N = N;
+            D = 0;
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+
+        }
 
     }
 }
--- SirsModel/Program.cs
+++ SirsModel/Program.cs
@@ -9,33 +9,64 @@ namespace SirsModel
     class Program
     {
 
-        public static double deltaT { get; set; }
-        public static double totalT { get; set; } = 0;
+        public static double deltaT { get; set; } = 1;
+        public static double totalT { get; set; } = 10;
         public static double sumT = 0;
-        public float k { get; set; } // fraction of infected group that will recover
-        public float b { get; set; }
-        public float deathRate { get; set; } // odds of an infected person dying after leaving the infected group (infectious, really)
-        public float borderTravelRate;
+        public static float k { get; set; } = .05F; // fraction of infected group that will recover
+        public static float b { get; set; } = 20;
+        public static float deathRate { get; set; } = .02F;// odds of an infected person dying after leaving the infected group (infectious, really)
+        public static float borderTravelRate = 01F;
+        public static List<GridUnit> activeGridUnits = new List<GridUnit>();
+        public static int gridXLength = 3;
+        public static int gridYLength = 3;
+        public static GridUnit[,] array = new GridUnit[gridXLength, gridYLength];
 
         static void Main(string[] args)
         {
+
+            // fill the array
+            
+            for (int i = 0; i < gridXLength; i++)
+            {
+                for (int j = 0; j < gridYLength; j++)
+                {
+                    array[i, j] = new GridUnit(10000, 0, 0, 10000, i, j);
+                }
+            }
+
+            array[1, 2].i = Convert.ToDouble(1000.0 / array[1, 2].N);
+            activeGridUnits.Add(array[1, 2]);
+
+        
+
+
+            // figure out the beginning GridUnit and add it to activeGridUnits
+
+
             while (sumT < totalT) 
             {
 
-
                 //for each grid, call doIteration if I is not 0;
+                for (int i = 0; i < activeGridUnits.Count - 1; i++)
+                {
+                    GridUnit temp = doIteration(activeGridUnits.ElementAt(i), deltaT);
+                    overWriteGrid(i, temp);
+                }
 
+                //for each grid, call calculateSpreadToNeighbors() on it
+                for (int i = 0; i <activeGridUnits.Count - 1; i++)
+                {
+                    calculateSpreadToNeighbors(activeGridUnits.ElementAt(i), borderTravelRate);
+                }
 
-                //for each grid, call calculateSpreadToNeighbors() on it and its right and then bottom neighbor after checking for edges
-
-
+                
                 sumT += deltaT;
             }
 
 
         }
 
-        public void doIteration(ref GridUnit grid, double deltaT)
+        public static GridUnit doIteration(GridUnit grid, double deltaT)
         {
 
             /*
@@ -69,42 +100,144 @@ namespace SirsModel
             // calculate losses
             long theDead = Convert.ToInt64(rPrime * deltaT * deathRate);
             grid.D += theDead;
-            grid.N -= theDead; // remove losses from total population, needs testing.
+            grid.N -= theDead; // remove losses from total population, needs testing.]
 
+            if (grid.I <= 0 || grid.N <= 0 || grid.R == grid.N)
+            {
+                activeGridUnits.Remove(grid);
+            }
 
+            return grid;
         }
 
-        public void calculateSpreadToNeighbors(ref GridUnit unitOne, ref GridUnit unitTwo, float borderTravelRate)
+        public static void calculateSpreadToNeighbors(GridUnit centerGrid, float borderTravelRate) // spreads infection out, one way, from each infected grid
         {
-            long infectedToOne = Convert.ToInt64(unitTwo.i * borderTravelRate * unitTwo.N); // actual number of people moving
-            long infectedToTwo = Convert.ToInt64(unitOne.i * borderTravelRate * unitOne.N);
 
-            long susceptableToOne = Convert.ToInt64(unitTwo.s * borderTravelRate * unitTwo.N);
-            long susceptableToTwo = Convert.ToInt64(unitOne.s * borderTravelRate * unitOne.N);
 
-            long recoveredToOne = Convert.ToInt64(unitTwo.r * borderTravelRate * unitTwo.N);
-            long recoveredToTwo = Convert.ToInt64(unitOne.r * borderTravelRate * unitOne.N);
-
+            long infectedOut = Convert.ToInt64(centerGrid.i * borderTravelRate * centerGrid.N);
+            long recoveredOut = Convert.ToInt64(centerGrid.r * borderTravelRate * centerGrid.N);
+            long susceptableOut = Convert.ToInt64(centerGrid.r * borderTravelRate * centerGrid.N);
 
             // readjust populations due to people leaving
-            long unitOneNTemp = unitOne.N;
-            unitOne.N = unitOne.N + (infectedToOne + susceptableToOne + recoveredToOne - infectedToTwo - susceptableToTwo - recoveredToTwo); 
-            unitTwo.N = unitTwo.N + (infectedToTwo + susceptableToTwo + recoveredToTwo - infectedToOne - susceptableToOne - recoveredToOne);
+            
+            // calculate each direction
+            if (centerGrid.yCoord <= 0) // go down
+            {
+                GridUnit target = array[centerGrid.xCoord, centerGrid.yCoord - 1];
+                if (target.N > 0)
+                {
+                    if (!(activeGridUnits.Contains(target)))
+                    {
+                        activeGridUnits.Add(target);
+                    }
 
+                    target.N += infectedOut + recoveredOut + susceptableOut;
+                    target.i += infectedOut / target.N;
+                    target.r += recoveredOut / target.N;
+                    target.s += susceptableOut / target.N;
 
-            // update fractions
-            unitOne.i += infectedToOne / unitOne.N;
-            unitTwo.i += infectedToTwo / unitTwo.N;
+                    overWriteGrid(target);
 
-            unitOne.r += recoveredToOne / unitOne.N;
-            unitTwo.r += recoveredToTwo / unitTwo.N;
+                    centerGrid.N -= infectedOut + recoveredOut + susceptableOut;
+                    centerGrid.i -= infectedOut / centerGrid.N;
+                    centerGrid.r -= recoveredOut / centerGrid.N;
+                    centerGrid.s -= susceptableOut / centerGrid.N;
 
-            unitOne.s += susceptableToOne / unitOne.N;
-            unitTwo.s += susceptableToTwo / unitTwo.N;
+                    overWriteGrid(centerGrid);
+                }
+            }
+            if (centerGrid.yCoord >= gridYLength - 1) // go up
+            {
+                GridUnit target = array[centerGrid.xCoord, centerGrid.yCoord + 1];
+                if (target.N > 0)
+                {
 
+                    if (!(activeGridUnits.Contains(target)))
+                    {
+                        activeGridUnits.Add(target);
+                    }
 
+                    target.N += infectedOut + recoveredOut + susceptableOut;
+                    target.i += infectedOut / target.N;
+                    target.r += recoveredOut / target.N;
+                    target.s += susceptableOut / target.N;
+
+                    overWriteGrid(target);
+
+                    centerGrid.N -= infectedOut + recoveredOut + susceptableOut;
+                    centerGrid.i -= infectedOut / centerGrid.N;
+                    centerGrid.r -= recoveredOut / centerGrid.N;
+                    centerGrid.s -= susceptableOut / centerGrid.N;
+
+                    overWriteGrid(centerGrid);
+                }
+            }
+            if (centerGrid.xCoord <= 0) // go left
+            {
+                GridUnit target = array[centerGrid.xCoord - 1, centerGrid.yCoord];
+                if (target.N > 0)
+                {
+                    if (!(activeGridUnits.Contains(target)))
+                    {
+                        activeGridUnits.Add(target);
+                    }
+
+                    target.N += infectedOut + recoveredOut + susceptableOut;
+                    target.i += infectedOut / target.N;
+                    target.r += recoveredOut / target.N;
+                    target.s += susceptableOut / target.N;
+
+                    overWriteGrid(target);
+
+                    centerGrid.N -= infectedOut + recoveredOut + susceptableOut;
+                    centerGrid.i -= infectedOut / centerGrid.N;
+                    centerGrid.r -= recoveredOut / centerGrid.N;
+                    centerGrid.s -= susceptableOut / centerGrid.N;
+
+                    overWriteGrid(centerGrid);
+                }
+            }
+            if (centerGrid.xCoord >= gridXLength - 1) // go right
+            {
+                GridUnit target = array[centerGrid.xCoord + 1, centerGrid.yCoord];
+                if (target.N > 0)
+                {
+                    if (!(activeGridUnits.Contains(target)))
+                    {
+                        activeGridUnits.Add(target);
+                    }
+
+                    target.N += infectedOut + recoveredOut + susceptableOut;
+                    target.i += infectedOut / target.N;
+                    target.r += recoveredOut / target.N;
+                    target.s += susceptableOut / target.N;
+
+                    overWriteGrid(target);
+
+                    centerGrid.N -= infectedOut + recoveredOut + susceptableOut;
+                    centerGrid.i -= infectedOut / centerGrid.N;
+                    centerGrid.r -= recoveredOut / centerGrid.N;
+                    centerGrid.s -= susceptableOut / centerGrid.N;
+
+                    overWriteGrid(centerGrid);
+                }
+            }
         }
-
-
+        public static void overWriteGrid(int i, GridUnit temp) 
+        {
+            activeGridUnits.ElementAt(i).N = temp.N;
+            activeGridUnits.ElementAt(i).i = temp.i;
+            activeGridUnits.ElementAt(i).r = temp.r;
+            activeGridUnits.ElementAt(i).s = temp.s;
+            activeGridUnits.ElementAt(i).D = temp.D;
+        } 
+        public static void overWriteGrid(GridUnit temp)
+        {
+            array[temp.xCoord, temp.yCoord].N = temp.N;
+            array[temp.xCoord, temp.yCoord].i = temp.i;
+            array[temp.xCoord, temp.yCoord].r = temp.r;
+            array[temp.xCoord, temp.yCoord].s = temp.s;
+            array[temp.xCoord, temp.yCoord].D = temp.D;
+        }
     }
 }
